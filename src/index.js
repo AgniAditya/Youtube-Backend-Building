0bfa467b5e1a855a07bc9e871f967d012735018c
AgniAditya <agniaditya05@gmail.com>
2025-07-27 00:24:05 +0530

    adding utils

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import dotenv from 'dotenv';
 import connectDB from "./db/index.js";
+import { app } from './app.js';
 
 dotenv.config({
     path : './env'
 })
 
 connectDB()
+.then(() => {
+    app.on("error", (error) => {
+        console.log("App not able to connect with database",error);
+        throw error;
+    })
+    app.listen(process.env.PORT || 3000, () => {
+        console.log(`Server is running at PORT: ${process.env.PORT || 3000}`);
+    })
+})
+.catch((err) => {
+    console.log("MongoDB connection FAILED !!",err)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,17 @@
+// Define a function that takes an Express route handler as input
+const asyncHandler = (requestHandler) => {
+
+    // Define a new middleware function that Express can use
+    // It takes req, res, and next just like any normal route handler
+    (req, res, next) => {
+
+        // Wrap the route handler in Promise.resolve()
+        // This ensures that even non-Promise values are handled as Promises
+        // If requestHandler throws or returns a rejected Promise, catch it and pass the error to Express
+        Promise.resolve(requestHandler()).catch((err) => next(err))
+    }
+
+}
+
+// Export the asyncHandler function for use in route files
+export { asyncHandler }
