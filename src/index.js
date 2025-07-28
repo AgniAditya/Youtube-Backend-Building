33884514bea403e1c3ab86d3dc81518e57f30688
AgniAditya <agniaditya05@gmail.com>
2025-07-28 17:16:34 +0530

    setup configured

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ connectDB()
         throw error;
     })
     app.listen(process.env.PORT || 3000, () => {
-        console.log(`Server is running at PORT: ${process.env.PORT || 3000}`);
+        console.log(`Server is running at -> http://localhost:${process.env.PORT || 3000}`);
     })
 })
 .catch((err) => {
--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -3,12 +3,12 @@ const asyncHandler = (requestHandler) => {
 
     // Define a new middleware function that Express can use
     // It takes req, res, and next just like any normal route handler
-    (req, res, next) => {
+    return (req, res, next) => {
 
         // Wrap the route handler in Promise.resolve()
         // This ensures that even non-Promise values are handled as Promises
         // If requestHandler throws or returns a rejected Promise, catch it and pass the error to Express
-        Promise.resolve(requestHandler()).catch((err) => next(err))
+        Promise.resolve(requestHandler(req,res,next)).catch((err) => next(err))
     }
 
 }
