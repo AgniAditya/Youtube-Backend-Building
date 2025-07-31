4d2b0b2d92ccbaa023e2546ebae7d2e0ec6c1bc0
AgniAditya <agniaditya05@gmail.com>
2025-07-31 16:56:49 +0530

    add: create subscription model with schema for user subscriptions

---
+++ src/models/subscriptions.model.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+    Subcriber: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    },
+    channel: {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User"
+    }
+},{timestamps:true})
+
+export const Subcription = mongoose.model("Subcription",subscriptionSchema)
