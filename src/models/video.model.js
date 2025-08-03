bc1036bfe5cdeb01d2191ef9c0d475d97bdd5698
AgniAditya <agniaditya05@gmail.com>
2025-08-03 20:15:48 +0530

    update : videoFile to videofile

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const videoSchema = new mongoose.Schema({
-    videoFile : {
+    videofile : {
         type : String, // Cloudinary url
         required : true
     },
