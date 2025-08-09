615a1bf63594a7d596010c13a5b45c8866752eb5
AgniAditya <agniaditya05@gmail.com>
2025-08-09 16:51:33 +0530

    implemente healthchek functionality to check health of the server

--- src/app.js
+++ src/app.js
@@ -18,10 +18,12 @@ app.use(cookieParser())
 import userRouter from './routes/user.routes.js';
 import commentRouter from './routes/comment.routes.js';
 import videoRouter from './routes/video.routes.js';
+import healthCheckerRouter from './routes/healthcheck.routes.js';
 
 //routes declaration
 app.use("/api/v1/users",userRouter)
 app.use("/api/v1/comments",commentRouter)
 app.use("/api/v1/video",videoRouter)
+app.use("/api/v1",healthCheckerRouter)
 
 export {app}
---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,21 @@
+import { apiError } from "../utils/apiError.js"
+import { apiResponse } from "../utils/apiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const healthcheck = asyncHandler(async (req, res) => {
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    try {
+        return res.status(200)
+        .json(new apiResponse(
+            200,
+            {
+                health: "Ok"
+            },
+            "Everything is fine"
+        ))
+    } catch (error) {
+        throw new apiError(500,error.message || "health is not ok")
+    }
+})
+
+export {healthcheck}
