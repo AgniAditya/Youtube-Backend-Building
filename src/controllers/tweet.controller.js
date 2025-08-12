50ca2273d8f1b97d3bdb181835ff3df0fab900e4
AgniAditya <agniaditya05@gmail.com>
2025-08-12 14:29:36 +0530

    implement createTweet functionality to post a tweet

---
+++ src/controllers/tweet.controller.js
@@ -0,0 +1,26 @@
+import {Tweet} from "../models/tweet.model.js"
+import {apiError} from "../utils/ApiError.js"
+import {apiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+const createTweet = asyncHandler(async (req, res) => {
+    const {content} = req.body
+    if(!content) throw new apiError(404,"tweet not found");
+
+    const tweet = await Tweet.create({
+        content: content,
+        owner: req.user
+    })
+    if(!tweet) throw new apiError(400,"not a valid user");
+
+    return res.status(200)
+    .json(new apiResponse(
+        200,
+        tweet,
+        "tweet is created successfully"
+    ))
+})
+
+export {
+    createTweet
+}
